08436219fa3bb2d13b49c2eb31a6f2e2faeae77a
Buda <145011829+pomidorchik03@users.noreply.github.com>
2025-11-29 17:07:26 +0700

    Update object.cpp

--- model/object.cpp
+++ model/object.cpp
@@ -3,7 +3,7 @@
 
 
 Object::Object() : name_("untitled"), center_(0,0) {}
-Object::Object(const std::string& name, const int x, const int y) : name_(name), pos_(x, y) {}
+Object::Object(const std::string& name, const int x, const int y) : name_(name), center_(x, y) {}
 
 void Object::setName(const std::string& name)
 {
